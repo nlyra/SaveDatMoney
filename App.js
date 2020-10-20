632fdcf7f4ba836a70d352e874e0bc0eb9c9c517
Federico Baron <federicoagustinbaron@gmail.com>
2020-10-20 00:10:43 -0400

    Get input fields

--- App.js
+++ App.js
@@ -2,19 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginPage from './src/pages/LoginPage';
-// import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 export default function App() {
   return (
-    <LoginPage />
-    // <Router >      
-    //   <Switch>        
-    //     <Route path="/" exact>          
-    //     <LoginPage />        
-    //     </Route>            
-    //     <Redirect to="/" />      
-    //   </Switch>      
-    // </Router>
+    //<LoginPage />
+    <Router >      
+      <Switch>        
+        <Route path="/" exact>          
+        <LoginPage />        
+        </Route>            
+        <Redirect to="/" />      
+      </Switch>      
+    </Router>
     // <View style={styles.container}>
     //   <Text>Open up App.js to start working on your app! Nate and fede wuz here</Text>
     //   <StatusBar style="auto" />
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login(){    
+
+    var loginName;    
+    var loginPassword; 
+
+    const [message,setMessage] = useState('');
+
     const doLogin = async event =>     
     {
         event.preventDefault();
-        alert('doIt()');    
+        alert('doIt() ' + loginName.value + ' ' + loginPassword.value );
     };    
     return(     
         <div id="loginDiv"> 
             <form onSubmit={doLogin}>
                 <span id="inner-title">PLEASE LOG IN</span>
                 <br />
-                <input type="text" id="loginName" placeholder="Username" />
-                <br />        
-                <input type="password" id="loginPassword" placeholder="Password" />
+                <input type="text" id="loginName" placeholder="Username"   
+                    ref={(c) => loginName = c} />
+                <input type="password" id="loginPassword" placeholder="Password"
+                    ref={(c) => loginPassword = c} />
                 <br />        
                 <input type="submit" id="loginButton" class="buttons" value = "Do It"          
                     onClick={doLogin} />        
             </form>        
-            <span id="loginResult"></span>     
+            <span id="loginResult">{message}</span>     
         </div>
     );
 };
