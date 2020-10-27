049b2449c3f1cfc3b5cd6215b0968eff7e6c99de
Pedro Castano <p_castano@knights.ucf.edu>
2020-10-27 14:53:12 -0400

    fixed mistake

--- src/pages/DashboardPage/InsightsPage/InsightsPage.js
+++ src/pages/DashboardPage/InsightsPage/InsightsPage.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import { firebase } from '../../firebase/config';
-import styles from './styles';
+import { firebase } from '../../../firebase/config';
+import styles from '../styles';
 
 
 export default function InsightsPage({navigation})
 {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Insights Page</Text>
         </View>
     )
--- src/pages/DashboardPage/SettingsPage/SettingsPage.js
+++ src/pages/DashboardPage/SettingsPage/SettingsPage.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import { firebase } from '../../firebase/config';
-import styles from './styles';
+import { firebase } from '../../../firebase/config';
+import styles from '../styles';
 
 
 export default function SettingsPage({navigation})
 {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Settings Page</Text>
         </View>
     )
