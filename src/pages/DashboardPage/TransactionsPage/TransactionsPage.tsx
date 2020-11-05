a011dcd3505b9e6f9270acb692042f1776d30f9b
javy3555 <javy3555@gmail.com>
2020-11-04 19:03:20 -0500

    Added addTransaction function

--- src/pages/DashboardPage/TransactionsPage/TransactionsPage.tsx
+++ src/pages/DashboardPage/TransactionsPage/TransactionsPage.tsx
@@ -8,9 +8,37 @@ import {format, subMonths, addMonths} from 'date-fns';
 import {IconButton} from 'react-native-paper';
 import MonthPicker from '../MonthPicker';
 
+
 export default function TransactionsPage({navigation})
 {
+
     const [date, setDate] = useState(new Date());
+    const [message, setMessage] = useState('');
+    const [cost, setCost] = useState('');
+    const [category, setCategory] = useState('');
+    const [transaction, setTransaction] = useState('');
+
+    const addTransaction = (e) => {
+
+        e.preventDefault();
+
+        firebase.auth().createUserWithEmailAndPassword(email, password).then((response) => {
+
+        const uid = response.user.uid
+            const data = {
+                id: uid,
+                cost,
+                category,
+                transaction
+            };
+
+            firebase.database().ref('users/' + uid).set(data).then(() => {
+                navigation.navigate('Login', data)
+            }).catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                setMessage(errorMessage);
+            });
 
     return (
         
@@ -36,7 +64,7 @@ export default function TransactionsPage({navigation})
                 <TouchableOpacity style={[{marginRight: '5%'}, buttons.long]} onPress={console.log("hi there")}>
                     <Text style={styles.buttonTitle}>Expenses</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={buttons.long} onPress={console.log("hi there")}>
+                <TouchableOpacity style={buttons.long} onPress={addTransaction}>
                     <Text style={styles.buttonTitle}>Income</Text>
                 </TouchableOpacity>
             </View>
