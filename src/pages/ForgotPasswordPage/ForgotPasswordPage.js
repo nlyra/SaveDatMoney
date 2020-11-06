27fe0b358210856ebb84479baaad2011f3295303
SchmitPSL <schmitpsl@yahoo.com>
2020-11-06 15:20:38 -0500

    Edited ForgotPassword

--- src/pages/ForgotPasswordPage/ForgotPasswordPage.js
+++ src/pages/ForgotPasswordPage/ForgotPasswordPage.js
@@ -1,82 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler';
-import { firebase } from '../../firebase/config'
 import styles from './styles';
 
-export default function RegistrationPage({navigation})
-{   
-    const [fullName, setFullName] = useState('')
+import { firebase } from '../../firebase/config'
+
+export default function ForgotPasswordPage({navigation})
+{
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('')
     const [message, setMessage] = useState('');
 
-    const onFooterLinkPress = () => {
-        navigation.navigate('Login')
-    }
+
     /**
+     *
      * @param {React.FormEvent<HTMLFormElement>} e
      */
-    const doRegister = (e) => 
-    {
-        e.preventDefault(); // to prevent webpage from realoading on submit each time login is pressed.
+    const doForgotPassword = (e) => {
 
-        // If passwords don't match
-        if (password !== confirmPassword) {
-            //alert("Passwords don't match.")
-            setMessage("Passwords don't match.");
-            return
-        }
+        e.preventDefault();
 
-        firebase.auth().createUserWithEmailAndPassword(email, password).then((response) => {
-            
+        firebase.auth().signInWithEmailAndPassword(email, password).then((response) => {
+
             const uid = response.user.uid
-            const data = {
-                id: uid,
-                email,
-                fullName,
-            };
+            const usersRef = firebase.database().ref('users/' + uid);
 
-            firebase.database().ref('users/' + uid).set(data).then(() => {
-                navigation.navigate('Login', data)
+            usersRef.once('value').then(function(snapshot) {
+                navigation.reset({
+                  index: 0,
+                  routes: [{ name: 'Budget' }],
+                });
+                //navigation.navigate('Dashboard', snapshot.val());
             }).catch((error) => {
                 var errorCode = error.code;
                 var errorMessage = error.message;
                 setMessage(errorMessage);
-            });
-            
-        }).catch(function(error) {
-            // Handle Errors here.
+            })
+        }).catch((error) => {
             var errorCode = error.code;
             var errorMessage = error.message;
             setMessage(errorMessage);
-          });
+        })
     }
 
-    return(
+    return (
         <View style={styles.container}>
-            <ScrollView style={{flex: 1, width: '100%'}} keyboardShouldPersistTaps="always">
+            <ScrollView style={{ flex: 1, width: '100%' }} keyboardShouldPersistTaps="always">
                 <Image
                     style={styles.logo}
                     source={require('../../../assets/savedatmoneylogo.png')}
                 />
                 <TextInput
                     style={styles.input}
-                    placeholder='E-mail adress'
+                    placeholder='E-mail'
                     placeholderTextColor="#aaaaaa"
-                    onChangeText={(text) => setFullName(text)}
-                    value={fullName}
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
                     underlineColorAndroid="transparent"
                     autoCapitalize="none"
                 />
                 {message ? <Text style={{ color: 'red' }}>{message}</Text> : null}
-                <TouchableOpacity style={styles.button} onPress={doRegister}>
-                    <Text style={styles.buttonTitle}>Create account</Text>
+                <TouchableOpacity style={styles.button} onPress={doForgotPassword}>
+                    <Text style={styles.buttonTitle}>Reset Password</Text>
                 </TouchableOpacity>
-                <View styles={styles.footerView}>
-                    <Text style={styles.footerText}>Already got an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Log in</Text></Text>
-                </View>
             </ScrollView>
         </View>
     )
