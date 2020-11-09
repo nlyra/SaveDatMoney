5cca16e304f078886d83f40368a9a158ff6c6af6
Pedro Castano <p_castano@knights.ucf.edu>
2020-11-09 16:46:58 -0500

    settings page skeleton in place

--- src/pages/DashboardPage/SettingsPage/SettingsPage.js
+++ src/pages/DashboardPage/SettingsPage/SettingsPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Image, Text, TextInput, TouchableOpacity, View, RefreshControl, Switch, Platform } from 'react-native';
+import { Alert, Image, Text, TextInput, TouchableOpacity, View, RefreshControl, Switch, Platform } from 'react-native';
 import { SettingsScreen, SettingsData } from 'react-native-settings-screen';
 import { ScrollView } from 'react-native-gesture-handler';
 import { firebase } from '../../../firebase/config';
@@ -12,90 +12,28 @@ export default function SettingsPage({navigation})
     const settingsData: SettingsData = [
       {
         type: 'SECTION',
-        header: 'My Section'.toUpperCase(),
-        footer:
-          'Donec sed odio dui. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.',
+        header: 'General',
         rows: [
           {
-            title: 'A row',
-            showDisclosureIndicator: true,
-          },
-          { title: 'A non-tappable row' },
-          {
-            title: 'This row has a',
-            subtitle: 'Subtitle',
+            title: 'Email',
+            subtitle: 'hardcoded@email.com',
             showDisclosureIndicator: true,
           },
           {
-            title: 'Long title. So long long long long long long long',
-            subtitle:
-              'And so is the subtitle. Even longer longer longer longer longer',
+            title: 'Currency',
+            renderAccessory: () => (
+              <Text style={{ color: '#999', marginRight: 6, fontSize: 18 }}>
+                $
+              </Text>
+            )
           },
           {
-            title: 'Switch',
+            title: 'Change Password',
+            showDisclosureIndicator: true,
+          },
+          {
+            title: 'Dark Mode',
             renderAccessory: () => <Switch value onValueChange={() => {}} />,
-          },
-          {
-            title: 'Text',
-            renderAccessory: () => (
-              <Text style={{ color: '#999', marginRight: 6, fontSize: 18 }}>
-                Maybe
-              </Text>
-            ),
-          },
-          {
-            title: 'Custom view',
-            renderAccessory: () => (
-              <View
-                style={{
-                  width: 30,
-                  height: 30,
-                  backgroundColor: 'blue',
-                }}
-              />
-            ),
-            showDisclosureIndicator: true,
-          },
-        ],
-      },
-      {
-        type: 'SECTION',
-        header: 'My Other Section'.toUpperCase(),
-        rows: [
-          {
-            title: 'Dolor Nullam',
-            showDisclosureIndicator: true,
-          },
-          {
-            title: 'Nulla vitae elit libero',
-            renderAccessory: () => (
-              <Text style={{ color: '#999', marginRight: 6, fontSize: 18 }}>
-                Dapibus
-              </Text>
-            ),
-          },
-          {
-            title: 'Ipsum Lorem Venenatis',
-            subtitle: 'Vestibulum Inceptos Fusce Justo',
-            renderAccessory: () => (
-              <Text style={{ color: '#999', marginRight: 6, fontSize: 18 }}>
-                Yes
-              </Text>
-            ),
-            showDisclosureIndicator: true,
-          },
-        ],
-      },
-      {
-        type: 'SECTION',
-        header: 'My Third Section'.toUpperCase(),
-        rows: [
-          {
-            title: 'Different title style',
-            showDisclosureIndicator: true,
-            titleStyle: {
-              color: 'red',
-            },
           },
         ],
       },
