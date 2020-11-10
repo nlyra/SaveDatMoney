ab34b44fb451baa19c9dc970a6586a5b6a4e24d0
battedtitan <54639746+battedtitan@users.noreply.github.com>
2020-11-09 19:14:42 -0500

    Revert "Merge branch 'settingsPage' into main"
    This reverts commit 50b9090584561a9dc5df579ee983c89ee7f18f51, reversing
    changes made to fea553398bf567f46f9a9d4c1b902f2f0678d1e4.

--- src/pages/DashboardPage/SettingsPage/SettingsPage.js
+++ src/pages/DashboardPage/SettingsPage/SettingsPage.js
@@ -1,61 +1,15 @@
 import React, { useState } from 'react';
-import { Alert, Image, Text, TextInput, TouchableOpacity, View, RefreshControl, Switch, Platform } from 'react-native';
-import { SettingsScreen, SettingsData } from 'react-native-settings-screen';
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { firebase } from '../../../firebase/config';
 import styles from '../styles';
 
-const fontFamily = Platform.OS === 'ios' ? 'Avenir' : 'sans-serif';
 
 export default function SettingsPage({navigation})
 {
-    const settingsData: SettingsData = [
-      {
-        type: 'SECTION',
-        header: 'General',
-        rows: [
-          {
-            title: 'Email',
-            subtitle: 'hardcoded@email.com',
-            showDisclosureIndicator: true,
-          },
-          {
-            title: 'Currency',
-            renderAccessory: () => (
-              <Text style={{ color: '#999', marginRight: 6, fontSize: 18 }}>
-                $
-              </Text>
-            )
-          },
-          {
-            title: 'Change Password',
-            showDisclosureIndicator: true,
-          },
-          {
-            title: 'Dark Mode',
-            renderAccessory: () => <Switch value onValueChange={() => {}} />,
-          },
-        ],
-      },
-    ]
-
     return (
         <View style={styles.container}>
-          <SettingsScreen
-            data={settingsData}
-            globalTextStyle={{ fontFamily }}
-            scrollViewProps={{
-              refreshControl: (
-                <RefreshControl
-                  refreshing={false}
-                  onRefresh={() => {
-                    this.setState({ refreshing: true })
-                    setTimeout(() => this.setState({ refreshing: false }), 3000)
-                  }}
-                />
-              ),
-            }}
-          />
+            <Text>Settings Page</Text>
         </View>
     )
 }
