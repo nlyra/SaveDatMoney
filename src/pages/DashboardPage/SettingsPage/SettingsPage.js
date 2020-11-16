04333f7062e78ae12b2e4ccaa5b3e8c4f10bc301
Pedro Castano <p_castano@knights.ucf.edu>
2020-11-16 14:32:57 -0500

    added some rows

--- src/pages/DashboardPage/SettingsPage/SettingsPage.js
+++ src/pages/DashboardPage/SettingsPage/SettingsPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Alert, Image, Text, TextInput, TouchableHighlight, View,
-         Switch, Platform, ScrollView, Modal, Span } from 'react-native';
+         Switch, Platform, ScrollView, Modal, FlatList } from 'react-native';
 import { firebase } from '../../../firebase/config';
 import styles from './styles';
 import { buttons, colors } from '../../stdStyles';
@@ -31,21 +31,19 @@ export default function SettingsPage({navigation})
 
     let theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme;
 
-    const toggleTheme = React.useCallback(() => {
-      return setIsThemeDark(!isThemeDark);
-    }, [isThemeDark]);
+    const toggleTheme = () => setIsThemeDark(!isThemeDark);
 
-    const preferences = React.useMemo(
-      () => ({
-        toggleTheme,
-        isThemeDark,
-      }),
-      [toggleTheme, isThemeDark]
-    );
+    // const preferences = React.useMemo(
+    //   () => ({
+    //     toggleTheme,
+    //     isThemeDark,
+    //   }),
+    //   [toggleTheme, isThemeDark]
+    // );
 
     const editEmail = () => {
       user.updateEmail(email);
-    }
+    };
 
     var user = firebase.auth().currentUser;
     var uid;
@@ -53,31 +51,24 @@ export default function SettingsPage({navigation})
 
     if (user != null) {
       uid = user.uid;
-    }
+    };
 
     var userRef = firebase.database().ref("users");
     userRef.orderByChild("id").equalTo(uid).on("child_added", function(snapshot) {
-         userData.push({
-             ...snapshot.val(),
+          userData.push({
+              ...snapshot.val(),
              key: snapshot.key,
-           });
+          });
           console.log("this is user data " + userData[0].fullName);
-     });
+          console.log(userData);
+    });
+
 
     return (
       <ScrollView>
         <View style={styles.modalView}>
           <Text style={styles.modalText}>Name</Text>
-          <TextInput
-            style={styles.input}
-            placeholderTextColor="black"
-            placeholder='Enter new name'
-            autoCapitalize="words"
-            onChangeText={(text) => setName(text)}
-            value={name}
-            underlineColorAndroid="transparent"
-            autoCapitalize="none"
-            />
+          <Text style={styles.textStyle}>     Pedro Castano</Text>
           <Text style={styles.modalText}>Change Password</Text>
           <TextInput
             style={styles.input}
@@ -89,10 +80,11 @@ export default function SettingsPage({navigation})
             autoCapitalize="none"
           />
           <Text style={styles.modalText}>Currency</Text>
-          <Text style={styles.textStyle}>    $</Text>
+          <Text style={styles.textStyle}>     $</Text>
           <Text style={styles.modalText}>Change Password</Text>
           <ScrollView contentContainerStyle={{
             flexDirection: "row",
+            marginTop: 15,
           }}>
             <Text style={styles.modalText}>Dark Mode</Text>
             <Switch
@@ -100,7 +92,7 @@ export default function SettingsPage({navigation})
                 marginLeft: 50,
               }}
               onValueChange={toggleTheme}
-              value={theme}
+              value={isThemeDark}
             />
           </ScrollView>
         </View>
