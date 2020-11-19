de023315fd9c3092670c36d2ecfcb4045a93fdf9
Devin Vanzant <t3helusivepanda@gmail.com>
2020-11-18 19:46:20 -0500

    Updated pull

---
+++ src/pages/DashboardPage/InsightsPage/BarGraph/graph.js
@@ -0,0 +1,18 @@
+import React, { useState} from 'react';
+import { Text, TouchableOpacity, View, Button, Alert, FlatList, StatusBar, TouchableHighlight, TextInput, Platform, Modal} from 'react-native';
+
+export default function BarGraph() {
+    return (
+        <View style = {style.main}>
+
+
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    main: {
+        marginVertical: 25,
+        marginHorizontal: 0,
+    }
+});
